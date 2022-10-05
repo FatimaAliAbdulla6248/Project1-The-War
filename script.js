86e9387aea88f0d482e94c0fc751e4214d7b8540
FatimaAliAbdulla6248 <fatimaradhi99@gmail.com>
2022-10-05 16:18:30 +0300

    trying to create comparision function

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 
-//  full deck of cards
+//full deck of cards
 //Total cards=52
 
-//const cards = {"dA":11 ,"dK":12,"dQ":13,"dJ":14,"d10":10,"d09":9,"d08":8,"d07":7,"d06":6,"d05":5,"d04":4,"d03":3,"d02":2,
-               //"hA":11,"hK":12,"hQ":13,"hJ":14,"h10":10,"h09":9,"h08":8,"h07":7,"h06":6,"h05":5,"h04":4,"h03":3,"h02":2,
-               //"sA":11,"sK":12,"sQ":13,"sJ":14,"s10":10,"s09":9,"s08":8,"s07":7,"s06":6,"s05":5,"s04":4,"s03":3,"s02":2,
-               //"cA":11,"cK":12,"cQ":13,"cJ":14,"c10":10,"c09":9,"c08":8,"c07":7,"c06":6,"c05":5,"c04":4,"c03":3,"c02":2}
+const cardsobjects = {"dA":14 ,"dK":13,"dQ":12,"dJ":11,"d10":10,"d09":9,"d08":8,"d07":7,"d06":6,"d05":5,"d04":4,"d03":3,"d02":2,
+                      "hA":14,"hK":13,"hQ":12,"hJ":11,"h10":10,"h09":9,"h08":8,"h07":7,"h06":6,"h05":5,"h04":4,"h03":3,"h02":2,
+                      "sA":14,"sK":13,"sQ":12,"sJ":11,"s10":10,"s09":9,"s08":8,"s07":7,"s06":6,"s05":5,"s04":4,"s03":3,"s02":2,
+                      "cA":14,"cK":13,"cQ":12,"cJ":11,"c10":10,"c09":9,"c08":8,"c07":7,"c06":6,"c05":5,"c04":4,"c03":3,"c02":2}
 
 const cards = ["dA","dK","dQ","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02",
                "hA","hK","hQ","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02",
               "sA","sK","sQ","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02",
               "cA","cK","cQ","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02"] 
-//split deck in half and randomize deck for two players
+
+ //split deck in half and randomize deck for two players
 //Each player has to have 26 cards
 
 
@@ -28,38 +29,40 @@ while(dealerDeck.length>26){
 console.log(dealerDeck)
 console.log(playerDeck)
 
-let dealerDeckindex=1;
-let playerDeckindex=1;
-let currentcard=document.getElementById('current card ')
+//Flip new card function 
+let playercard =document.querySelector('#CurrentCardP')//html id
+let dealercard =document.querySelector('#CurrentCardD')//html id
 
-// Add event listeners to your html buttons/cards
-// pick a random card from player deck
-   // let playerCardUp = ??????
-    // add that card to the up deck for player
-    // pick a random card from dealer deck
-    //let dealerCardUp = ??????
-    // add that card to the up deck for dealer
-function flipNewCard(){
-    currentcard.setAttribute("src",dealerDeck[dealerDeckindex])
-    dealerDeckindex++
-    if (dealerDeckindex>26){dealerDeckindex=0;}   
-    
+    function flipNewCard(e){
+        console.log(playerDeck)
+        console.log(dealerDeck)
+        console.log('the value of the Player card is: ', cardsobjects[playerDeck[0]])
+        console.log('the value of the Dealer card is: ', cardsobjects[dealerDeck[0]])
+        //Empty class each time 
+        playercard.className = ''
+        dealercard.className=''
+        //Add new card from css images
+        playercard.classList.add(`card`)
+        dealercard.classList.add(`card`)
+        //take the first card index from the deck
+        playercard.classList.add(`${playerDeck[0]}`)
+        dealercard.classList.add(`${dealerDeck[0]}`)
+
+        //delete the card from the deck
+        playerDeck.shift()
+        dealerDeck.shift()     
 }
 
+
+ 
+//Add event listener to click the back red card 
 let drawDecks = document.querySelectorAll('.back-red')
 drawDecks.forEach(deck=>deck.addEventListener("click", flipNewCard))
  
-
-
-
-
-
-
-
-// create functions for each event listener
+ 
 //compare the cards function 
 //score the winner function 
-
-// for ex. cardClick ---> compare the two cards, find winner, add score point, change css/card image class
+ 
+ 
 
 //resetbutton
